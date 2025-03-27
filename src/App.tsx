f4bcebae41d4c599614212639070c64b74d111d7
Vedant Pathare <patharevedant@gmail.com>
2025-03-27 23:42:28 +0530

    Added Bamboo Model

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,11 @@ const models = [
     name: 'Tulsi',
     url: 'models/tulsi/Tulsi_Vedant_intermediate_attempt_Model_2.glb',
     info: 'Tulsi, also known as Holy Basil, is a plant species of the genus Ocimum. It is native to India and Southeast Asia and grows well in tropical and subtropical climates around the world. It is cultivated for its medicinal, religious, and culinary uses, with a rich history in Ayurvedic medicine for promoting health and wellness.'
+  },
+  {
+    name: 'Bamboo',
+    url: 'models/bamboo/Bamboo_Vedant_first_attempt_Model_4.glb',
+    info: 'Bamboo is a fast-growing, woody-stemmed plant belonging to the grass family Poaceae. It is native to Asia, particularly East and Southeast Asia, but is found in tropical, subtropical, and temperate regions across the globe. Bamboo thrives in a variety of climates and soil conditions, making it one of the most adaptable plants. It is widely cultivated for its versatile uses in construction, crafts, food, and landscaping. In many cultures, bamboo holds cultural, economic, and ecological significance, and has been valued in traditional medicine, daily utilities, and as a sustainable resource.'
   }
 ];
 
